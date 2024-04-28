b17730ed066df3291121954ac33950f63b733e2f
pikselarius <tugurl.bakan@gmail.com>
2024-04-28 04:44:34 +0300

    +Safe Zone +DragDropEdit

--- Assets/Scripts/DragDrop2.cs
+++ Assets/Scripts/DragDrop2.cs
@@ -6,23 +6,52 @@ public class DragDrop2 : MonoBehaviour
 {
     public GameObject Canvas;
     public GameObject SafeZone;
+    public GameObject ArenaSafeZone;
 
     private bool isDragging = false;
+    private GameObject startParent;
+    private Vector2 startPosition;
+    private GameObject safeZone;
+    private bool isOverSafeZone;
 
     void Start()
     {
         Canvas = GameObject.Find("Main Canvas");
         SafeZone = GameObject.Find("SafeZone");
+        ArenaSafeZone = GameObject.Find("ArenaSafeZone");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverSafeZone = true;
+        safeZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverSafeZone = false;
+        safeZone = null;
     }
 
     public void StartDrag()
     {
         isDragging = true;
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
     }
 
     public void EndDrag()
     {
         isDragging = false;
+        if (isOverSafeZone)
+        {
+            transform.SetParent(safeZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
     }
 
     void Update()
